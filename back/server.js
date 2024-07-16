aeb32bdf9d6c0ab6d2261dcda6efa25cb979bf05
santhosh <santhoshm160502@gmail.com>
2024-07-16 11:05:49 +0530

    sss

--- back/server.js
+++ back/server.js
@@ -40,11 +40,11 @@ const upload = multer({ storage: storage });
 const uploadExcel = multer({storage: storageExcel});
 
 const db = mysql.createPool({
-    // host: "127.0.0.1",
-    // port: "3306",
-    // user: "root",
-    // password: "Alliswell@12",
-    // database: "school"
+//     host: "127.0.0.1",
+//     port: "3306",
+//     user: "root",
+//     password: "Alliswell@12",
+//     database: "school"
 
     host:'193.203.184.74',
     port:"3306",
